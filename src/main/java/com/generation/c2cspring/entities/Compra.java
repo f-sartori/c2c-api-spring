b2479bb2eba3b66669f2c23eb3e9fe878f565285
Guilhermegit-code <guizolas.52@gmail.com>
2020-02-04 15:35:19 -0300

    api interligado com o front-end exceto o delete

--- src/main/java/com/generation/c2cspring/entities/Compra.java
+++ src/main/java/com/generation/c2cspring/entities/Compra.java
@@ -1,13 +1,11 @@
 package com.generation.c2cspring.entities;
 
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
